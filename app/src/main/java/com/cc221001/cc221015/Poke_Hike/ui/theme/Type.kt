0eb3ccfdbc950487aa50a540f1475b4abe55323b
elina <117643112+thisis-elina@users.noreply.github.com>
2024-01-19 20:30:41 +0100

    new trainers

--- app/src/main/java/com/cc221001/cc221015/Poke_Hike/ui/theme/Type.kt
+++ app/src/main/java/com/cc221001/cc221015/Poke_Hike/ui/theme/Type.kt
@@ -2,14 +2,17 @@ package com.cc221001.cc221015.Poke_Hike.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.cc221001.cc221015.Poke_Hike.R
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = FontFamily(Font(R.font.aldrich)),
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
