07386d684c8cf5b8bee9aabda2fef9e5ec9cd05d
elina <117643112+thisis-elina@users.noreply.github.com>
2024-01-20 17:08:23 +0100

    new font

--- app/src/main/java/com/cc221001/cc221015/Poke_Hike/composables/MainComposables.kt
+++ app/src/main/java/com/cc221001/cc221015/Poke_Hike/composables/MainComposables.kt
@@ -13,44 +13,49 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.cc221001.cc221015.Poke_Hike.viewModel.MainViewModel
 
 // Helper Composable function for displaying errors.
 @Composable
-fun ErrorScreen(){
+fun ErrorScreen() {
     Text(text = "Looks like you didn't create your user yet, please make sure to create one before using PokeHike!")
 }
 
 // Helper Composable function for displaying text.
 @Composable
-fun TextBox(text:String){
+fun TextBox(text: String) {
     Text(text = text)
 }
 
 @Composable
-fun mainScreen(mainViewModel: MainViewModel){
+fun mainScreen(mainViewModel: MainViewModel) {
 // Using a Column to layout elements vertically.
     Column() {
         // A Row for displaying the title, with dynamic text based on the 'favorite' flag
         // A Row to display the list of Pokemon.
-        Row (modifier = Modifier
-            .clip(RoundedCornerShape(topStart = 20.dp, topEnd= 20.dp, bottomEnd = 0.dp, bottomStart=0.dp))){
+        Row(
+            modifier = Modifier
+                .clip(
+                    RoundedCornerShape(
+                        topStart = 20.dp,
+                        topEnd = 20.dp,
+                        bottomEnd = 0.dp,
+                        bottomStart = 0.dp
+                    )
+                )
+        ) {
             // Calling PokemonList Composable to display the actual list.
             ContentList()
         }
@@ -61,17 +66,20 @@ fun mainScreen(mainViewModel: MainViewModel){
 fun ContentList() {
     // LazyColumn is used for efficiently displaying a list that can be scrolled.
     // It only renders the items that are currently visible on screen.
-    LazyColumn (modifier= Modifier
-        .background(color = Color(0, 0, 0, 125))
-        .padding(top = 20.dp)
-        .fillMaxSize()
-    ){
-        item{
-            CustomContainer(200, 200, 20, null)
-            CustomSplitter(w = 500, h = 2, p = 20 )
+    LazyColumn(
+        modifier = Modifier
+            .background(color = Color(0, 0, 0, 125))
+            .padding(top = 20.dp)
+            .fillMaxSize()
+    ) {
+        item {
+            CustomContainerBlurry(200, 200, 20, "This is Text", null)
+            CustomSplitter(w = 500, h = 2, p = 20)
             CustomContainerTransparent(w = 500, h = 300, p = 20, null)
-            Box(modifier=Modifier.fillMaxWidth(),
-                contentAlignment = Center) {
+            Box(
+                modifier = Modifier.fillMaxWidth(),
+                contentAlignment = Center
+            ) {
                 CustomButton(
                     text = "Create Trainer",
                     onClick = { /*TODO*/ },
@@ -84,17 +92,22 @@ fun ContentList() {
 }
 
 @Composable
-fun CustomSplitter(w:Int,h:Int,p:Int){
-    Surface(modifier = Modifier
-        .width(w.dp)
-        .height(h.dp)
-        .padding(horizontal = p.dp),
-        color =Color(255, 255, 255, 50)){}
+fun CustomSplitter(w: Int, h: Int, p: Int) {
+    Surface(
+        modifier = Modifier
+            .width(w.dp)
+            .height(h.dp)
+            .padding(horizontal = p.dp),
+        color = Color(255, 255, 255, 50)
+    ) {}
 }
+
 @Composable
-fun CustomContainerTransparent(w:Int, h:Int, p:Int, content: @Composable() (() -> Unit?)?){
-    Box(modifier=Modifier.fillMaxWidth(),
-        contentAlignment = Center)
+fun CustomContainerTransparent(w: Int, h: Int, p: Int, content: @Composable() (() -> Unit?)?) {
+    Box(
+        modifier = Modifier.fillMaxWidth(),
+        contentAlignment = Center
+    )
     {
         Box(
             modifier = Modifier
@@ -110,9 +123,11 @@ fun CustomContainerTransparent(w:Int, h:Int, p:Int, content: @Composable() (() -
 }
 
 @Composable
-fun CustomContainer(w:Int, h:Int, p:Int, content: @Composable() (() -> Unit?)?){
-    Box(modifier=Modifier.fillMaxWidth(),
-        contentAlignment = Center)
+fun CustomContainer(w: Int, h: Int, p: Int, content: @Composable() (() -> Unit?)?) {
+    Box(
+        modifier = Modifier.fillMaxWidth(),
+        contentAlignment = Center
+    )
     {
         Surface(
             modifier = Modifier
@@ -121,7 +136,7 @@ fun CustomContainer(w:Int, h:Int, p:Int, content: @Composable() (() -> Unit?)?){
                 .padding(p.dp)
                 .clip(RoundedCornerShape(10.dp))
                 .border(2.dp, Color(255, 255, 255, 75), RoundedCornerShape(10.dp)),
-            color=Color(255,255,255,50)
+            color = Color(255, 255, 255, 50)
         ) {
             if (content != null) {
                 content()
@@ -131,14 +146,69 @@ fun CustomContainer(w:Int, h:Int, p:Int, content: @Composable() (() -> Unit?)?){
 }
 
 @Composable
-fun CustomHeadline(text:String){
-    Box(modifier=Modifier.fillMaxWidth(),
-        contentAlignment = Center){
+fun CustomContainerBlurry(
+    w: Int,
+    h: Int,
+    p: Int,
+    text: String,
+    content: @Composable (() -> Unit)?
+) {
+    // Wrap the entire CustomContainerBlurry with Cloudy
+    Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Center) {
+        // Cloudy with blurred container
+        Box(
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Center
+        ) {
+            // Moved content inside the Surface
+            Surface(
+                modifier = Modifier
+                    .width(w.dp)
+                    .height(h.dp)
+                    .padding(p.dp)
+                    .clip(RoundedCornerShape(10.dp))
+                    .blur(10.dp)
+                    .border(2.dp, Color(255, 255, 255, 75), RoundedCornerShape(10.dp)),
+                color = Color(255, 255, 255, 50)
+            ) {
+                    if (content != null) {
+                        content()
+                    }
+                }
+            }
+        // Text outside the blurred container with a semi-transparent background
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = text,
+                modifier = Modifier
+                    .fillMaxSize(),
+                color = Color.White,
+                textAlign = TextAlign.Center,
+            )
+        }
+    }
+}
+
+
+
+
+@Composable
+fun CustomHeadline(text: String) {
+    Box(
+        modifier = Modifier.fillMaxWidth(),
+        contentAlignment = Center
+    ) {
         Text(text = text)
     }
 }
+
 @Composable
-fun CustomButton(text: String, onClick: () -> Unit, amount:Int, amount2:Int) {
+fun CustomButton(text: String, onClick: () -> Unit, amount: Int, amount2: Int) {
     Surface(
         color = Color(106, 84, 141, 255),
         shape = RoundedCornerShape(10.dp),
