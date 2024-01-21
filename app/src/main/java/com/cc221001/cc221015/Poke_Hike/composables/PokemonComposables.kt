8f9a57fc3fdc89cfb043a0864ab6aef07215b981
elina <117643112+thisis-elina@users.noreply.github.com>
2024-01-21 22:30:34 +0100

    chnaged colours for liked and owned

--- app/src/main/java/com/cc221001/cc221015/Poke_Hike/composables/PokemonComposables.kt
+++ app/src/main/java/com/cc221001/cc221015/Poke_Hike/composables/PokemonComposables.kt
@@ -224,7 +224,7 @@ fun PokemonItem(pokemon: Pokemon?, pokemonViewModel: PokemonViewModel, favorite:
                         pokemonViewModel.likePokemon(pokemon, favorite)
                     }
                 }) {
-                    val tint = if (pokemon.liked == "true") Color.Red else Color.Gray
+                    val tint = if (pokemon.liked == "true") Color(200, 84, 141, 255) else Color.Gray
                     Icon(Icons.Default.Favorite, "Like", tint = tint, modifier = Modifier.size(20.dp))
                 }
             }
@@ -236,7 +236,7 @@ fun PokemonItem(pokemon: Pokemon?, pokemonViewModel: PokemonViewModel, favorite:
             .weight(0.5f),
             contentAlignment = Alignment.Center) {
             if (pokemon != null) {
-                val tint = if (pokemon.owned == "true") Color.Green else Color.Gray
+                val tint = if (pokemon.owned == "true") Color(106, 84, 141, 255) else Color.Gray
                 Icon(Icons.Default.CheckCircle, "Like", tint = tint, modifier = Modifier.size(20.dp))
             }
         }
